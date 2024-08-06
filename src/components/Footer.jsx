ea863c688f5173ab640235cc180864d2b20a2088
Tuhin Ahamed <mernstackprogramming@gmail.com>
2024-08-06 12:48:35 +0600

    create footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='bg-white py-4 mt-4 xl:mt-8'>
+      <p className='text-center mx-auto capitalize'>©️2024 by <span className="font-medium text-bluePrimary">e-book</span> | all right reserved</p>
+    </footer>
   )
 }
 
